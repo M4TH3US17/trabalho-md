952b1f79410626febaa619d3a1c36cd8b69a0cfa
M4TH3US17 <matheusdalvino50@gmail.com>
2026-06-30 13:45:12 -0400

    feat: problema 2

---
+++ problema-2/codigo-corrigido.py
@@ -0,0 +1,17 @@
+# @author: Matheus Washington Dalvino Marques
+# @date: 2026-06-30
+
+def soma_pares_corrigida(n):
+    assert n >= 0, "Erro: Pré-condição violada!"
+    s = 0
+    i = 0
+    assert s == i * (i + 1) and 0 <= i <= n
+    while i < n:
+        velha_variante = n - i
+        assert velha_variante >= 0
+        s = s + (2 * i + 2)
+        i = i + 1
+        assert s == i * (i + 1) and 0 <= i <= n
+        assert (n - i) < velha_variante
+    assert s == n * (n + 1)
+    return s
---
+++ problema-2/codigo-original-instrumentado-com-erro.py
@@ -0,0 +1,16 @@
+# @author: Matheus Washington Dalvino Marques
+# @date: 2026-06-30
+
+def soma_pares_errada(n):
+    assert n >= 0, "Erro: Pré-condição violada!"
+    s = 0
+    i = 0
+    assert s == i * (i + 1) and 0 <= i <= n
+    while i < n:
+        velha_variante = n - i
+        i = i + 1
+        s = s + (2 * i) 
+        assert s == i * (i + 1) and 0 <= i <= n
+        assert (n - i) < velha_variante
+    assert s == n * (n + 1)
+    return s
